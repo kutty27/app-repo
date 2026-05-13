0c058db981fb43c4aa46edc0acb7f6f11239db5d
kutty27 <154492609+kutty27@users.noreply.github.com>
2026-05-13 17:03:25 +0530

    Update home route message to reflect version 2

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "AI Assisted GitOps Pipeline Running Successfully"
+    return "Version 2 deployed successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
